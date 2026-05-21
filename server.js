d9dd68763822b8fdd1b4bf149f970b8fe5d5f0e8
Student Developer <student@example.com>
2026-05-21 19:09:16 +0600

    Hardcode CORS origin for testing

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'https://studynook-client-pi.vercel.app',
     credentials: true,
   })
 );
